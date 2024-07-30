69eb8ece100e2c3e26e05dbd73a6ec4aba9b2825
솜놜 <90139018+somnwal@users.noreply.github.com>
2024-07-30 16:38:35 +0900

    Initial commit

---
+++ shopping/shopping/shoppingApp.swift
@@ -0,0 +1,17 @@
+//
+//  shoppingApp.swift
+//  shopping
+//
+//  Created by CHOI on 7/30/24.
+//
+
+import SwiftUI
+
+@main
+struct shoppingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
